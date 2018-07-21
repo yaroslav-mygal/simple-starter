40e92e8a3459babb15badd0317c43ce69f22525a
Ярослав <migal.yaroslav@gmail.com>
2018-07-21 20:46:29 +0300

    bootstrap reset

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,19 @@
 
 const gulp = require("gulp"),
   sass = require("gulp-sass"),
-  browserSync = require("browser-sync").create();
-const srcPath = "src";
-const devPath = "dev";
-const distPath = "dist";
+  browserSync = require("browser-sync").create(),
+  clean = require("gulp-clean"),
+  autoprefixer = require("gulp-autoprefixer"),
+  sourcemaps = require("gulp-sourcemaps"),
+  useref = require("gulp-useref");
+
+// path
 const dir = {
   srcPath: "src",
   devPath: "dev",
   distPath: "dist"
 };
+
 const path = {
   src: {
     root: dir.srcPath,
@@ -44,24 +48,46 @@ const path = {
   }
 };
 
+// settings
+const autoprefixerSettings = {
+  browsers: ["last 6 versions"],
+  cascade: false
+};
 // tasks
 
+gulp.task("clean", function(cb) {
+  var folderName = process.env.NODE_ENV === "production" ? "dist" : "dev";
+  return gulp.src(folderName, { read: false }).pipe(clean());
+});
+
+gulp.task("enter-prod", function() {
+  process.env.NODE_ENV = "production";
+});
+gulp.task("enter-dev", function() {
+  process.env.NODE_ENV = "development";
+});
+
+gulp.task("html", function() {
+  return gulp
+    .src(path.src.html)
+    .pipe(
+      useref({
+        noconcat: true
+      })
+    )
+    .pipe(gulp.dest(path.dev.html))
+    .pipe(browserSync.stream());
+});
+
 gulp.task("sass", function() {
-  return (
-    gulp
-      .src(path.src.styles)
-      // .pipe(sourcemaps.init())
-      // .pipe(sass().on("error", sass.logError))
-      // .pipe(
-      //   autoprefixer({
-      //     browsers: ["last 6 versions"],
-      //     cascade: false
-      //   })
-      // )
-      // .pipe(sourcemaps.write("../css"))
-      .pipe(gulp.dest(path.dev.styles))
-      .pipe(browserSync.stream())
-  );
+  return gulp
+    .src(path.src.styles)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(autoprefixer(autoprefixerSettings))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest(path.dev.styles))
+    .pipe(browserSync.stream());
 });
 
 gulp.task("dist-sass", function() {
@@ -91,10 +117,19 @@ gulp.task("watch", function() {
 gulp.watch(path.src.img, ["images"]);
 // gulp.watch(path.src.spriteSource, ["sprite"]);
 // gulp.watch(path.src.fonts, ["fonts"]);
-gulp.watch(path.src.root + "/assets/css/**/*.scss", ["sass"]);
-// gulp.watch(path.src.html, ["html"]);
+gulp.watch(path.src.root + "/assets/scss/**/*.scss", ["sass"]);
+gulp.watch(path.src.html, ["html"]);
 // gulp.watch(path.src.htmlIncludes, ["html"]);
 // gulp.watch(path.src.js, ["js"]);
 
 gulp.task("default", ["watch"]);
-gulp.task("dev", ["sass"]);
+
+gulp.task("dev", ["enter-dev", "clean"], function() {
+  gulp.start(["build"]);
+});
+
+gulp.task("dist", ["enter-prod", "clean"], function() {
+  gulp.start(["build"]);
+});
+
+gulp.task("build", ["sass", "html"]);
